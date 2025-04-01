f44677477271a2b75f1a1a8d161bff49be5982ed
= <alecx315@gmail.com>
2025-03-31 21:09:16 -0400

    Moved main class into a package

---
+++ src/main/java/com/hunter/budget/entity/Category.java
@@ -0,0 +1,28 @@
+package com.hunter.budget.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Table(name = "category")
+@Data
+public class Category {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @JoinColumn(name = "parent_id")
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Category parent;
+
+    @OneToMany(cascade = CascadeType.ALL
+    , fetch = FetchType.LAZY, mappedBy = "parent")
+    private List<Category> subCategories;
+}
