7123096b069878eca887e7799b1c29091ceb935a
Alecx Hunter <alecx315@gmail.com>
2025-04-02 18:05:39 -0400

    Update config and packages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,23 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  root: 'src',
+    build: {
+        outDir: '../dist',
+        sourcemap: true
+    },
+    server: {
+      proxy: {
+        '/api': 'http://localhost:8080'
+      }
+    },
+    define: {
+      "__API_URL__": "'http://localhost:8080'"
+    },
+    plugins: [
+        react({
+          // Use React plugin in all *.jsx and *.tsx files
+          include: '**/*.{jsx,tsx}',
+        }),
+    ],
 })
